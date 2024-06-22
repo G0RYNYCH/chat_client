8cc716f5b935703a1c15aae79b3646e2acc56448
G0RYNYCH <the.garynych@gmail.com>
2024-06-23 00:09:09 +0200

    connect to chat ad start connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import { WaitingRoom } from "./components/WaitingRoom";
+import {HubConnectionBuilder} from "@microsoft/signalr";
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-grey-100">
-      <WaitingRoom />
-    </div>
+    const joinChat = async (userName, chatRoomName) => {
+        var connection = new HubConnectionBuilder()
+            .withUrl("http://localhost:5024/chat")
+            .withAutomaticReconnect()
+            .build();
+
+        try {
+            await connection.start();
+            await connection.invoke("JoinChat", {userName, chatRoomName});
+            console.log(connection);
+        }catch (error){
+            console.log(error)
+        }
+    }
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-grey-100">
+            <WaitingRoom joinChat={joinChat}/>
+        </div>
   );
 }
 

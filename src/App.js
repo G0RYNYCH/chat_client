b5222efecc9bab945ca20d50249b8dd75f1a06e4
G0RYNYCH <the.garynych@gmail.com>
2024-06-29 00:57:22 +0200

    in progress. chat component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { WaitingRoom } from "./components/WaitingRoom";
 import {HubConnectionBuilder} from "@microsoft/signalr";
+import {useState} from "react";
 
 function App() {
+    const [connection, setConnection] = useState(null)
+
     const joinChat = async (userName, chatRoomName) => {
         var connection = new HubConnectionBuilder()
             .withUrl("http://localhost:5024/chat")
             .withAutomaticReconnect()
             .build();
 
+        connection.on("ReceiveMessage", (userName, message) => {
+            console.log(userName);
+            console.log(message);
+        })
+
         try {
             await connection.start();
             await connection.invoke("JoinChat", {userName, chatRoomName});
+            setConnection(connection)
+
             console.log(connection);
         }catch (error){
             console.log(error)

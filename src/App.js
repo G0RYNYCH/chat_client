29aaf6c1123b5447f8315a7bda5b8851b9a27ae7
G0RYNYCH <the.garynych@gmail.com>
2024-06-29 23:26:09 +0200

    add message component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { WaitingRoom } from "./components/WaitingRoom";
 import {HubConnectionBuilder} from "@microsoft/signalr";
 import {useState} from "react";
+import {Chat} from "./components/Chat";
 
 function App() {
     const [connection, setConnection] = useState(null)
+    const [chatRoomName, setChatRoomName] = useState("")
+    const [messages, setMessages] = useState([])
 
     const joinChat = async (userName, chatRoomName) => {
         var connection = new HubConnectionBuilder()
@@ -12,23 +15,25 @@ function App() {
             .build();
 
         connection.on("ReceiveMessage", (userName, message) => {
-            console.log(userName);
-            console.log(message);
+            setMessages((messages) => [...messages, {userName, message}]);
         })
 
         try {
             await connection.start();
             await connection.invoke("JoinChat", {userName, chatRoomName});
-            setConnection(connection)
-
-            console.log(connection);
+            setConnection(connection);
+            setChatRoomName(chatRoomName);
         }catch (error){
             console.log(error)
         }
     }
     return (
         <div className="min-h-screen flex items-center justify-center bg-grey-100">
-            <WaitingRoom joinChat={joinChat}/>
+            {connection ? (
+                <Chat messages={messages} chatRoomName={chatRoomName}/>
+            ) : (
+                <WaitingRoom joinChat={joinChat}/>
+            )}
         </div>
   );
 }
